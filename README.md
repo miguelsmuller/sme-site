0f44b363abf77d9e78e7503948cffe49a482cd38
Miguel <M>
2020-06-15 22:57:55 -0300

    Remoção de depências e correção do formulário de contato e da listagem de notícias

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # **SME-SITE**
 > Site da Secretaria Municipal de Educação do Município de Rio Claro -  Site da Secretaria Municipal de Educação do Município de Rio Claro - Site da Secretaria Municipal de Educação do Município de Rio Claro - Site da Secretaria Municipal de Educação do Município de Rio Claro - Site da Secretaria Municipal de Educação do Município de Rio Claro - Site da Secretaria Municipal de Educação do Município de Rio Claro - Site da Secretaria Municipal de Educação do Município de Rio Claro - Site da Secretaria Municipal de Educação do Município de Rio Claro
 
-**Versão Estável:** 1.0  
+**Versão Estável:** 1.1  
 **Licensa:** Proprietário - Usu Privado  
 Todos os direitos reservados.  
 É estritamente proibida a cópia não autorizada de qualquer arquivo deste projeto, por qualquer meio.  
@@ -67,5 +67,8 @@ git diff --cached
 - **[Comandos úteis do git](https://gist.github.com/leocomelli/2545add34e4fec21ec16)**
 
 ## **Changelog**  
+= **1.2.0 - 15/06/2020** =  
+Remoção de depências e correção do formulário de contato e da listagem de notícias  
+
 = **1.0.0 - 14/06/2020** =  
 Projeto Inicial  
--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, retry } from 'rxjs/operators';
 import { Noticia } from '@app/interfaces';
 import { environment } from '@env/environment';
 
@@ -9,32 +9,57 @@ import { environment } from '@env/environment';
   providedIn: 'root'
 })
 export class Api {
-  apiNews = environment.apiNews;
-  apiEmail = environment.apiEmail;
+  apiNewsOptions = environment.apiNews;
+  apiEmailOptions = environment.apiEmail;
 
   constructor(
-    private http: HttpClient
+    private serviceRequest: HttpClient
   ) { }
 
   getNews(): Observable<Noticia[]> {
-    const apiHeaders = new HttpHeaders().set('Access-Control-Allow-Origin', '*');
-    return this.http.get<Noticia[]>(this.apiNews.url, { headers: apiHeaders });
+    const httpUrl = `${this.apiNewsOptions.url}/posts`;
+
+    let  httpParams = new HttpParams();
+    httpParams = httpParams.append('tags', this.apiNewsOptions.tag);
+    httpParams = httpParams.append('per_page', this.apiNewsOptions.count);
+    httpParams = httpParams.append('_fields', 'title, link, date');
+
+    return this.serviceRequest.get<Noticia[]>(httpUrl, { params: httpParams }).pipe(
+      retry(1),
+      catchError(error => {
+        return throwError(`Error Code: ${error.status}\nMessage: ${error.message}`);
+      })
+    );
   }
 
   sendEmail(data): Observable<any> {
-    const url = `${this.apiEmail.url}/messages`;
+    const httpUrl = `${this.apiEmailOptions.url}/messages`;
 
-    const apiHeaders = new HttpHeaders({
-      enctype: 'multipart/form-data',
-      Authorization: `Basic ${this.apiEmail.key}`
-    });
+    let apiHeaders = new HttpHeaders();
+    apiHeaders = apiHeaders.append('Authorization', 'Basic ' + btoa(this.apiEmailOptions.key));
 
-    const formData = new FormData();
-    formData.append('from', this.apiEmail.emailFrom);
-    formData.append('to', data.destinoMensagem);
-    formData.append('subject', data.tipoManifestacao);
-    formData.append('text', data.nomeSolicitante + ' - ' + data.distrito + ' - ' + data.telefone + ' - ' + data.mensagem);
+    const bodyFromName = data.nomeSolicitante;
+    const bodyFromEmail = data.email;
+    const bodyMessageType = data.tipoManifestacao;
+    const bodyMessageDestiny = data.destinoMensagem;
+    const bodyMessageDiscricty = data.distrito;
+    const bodyMessagePhone = data.telefone;
+    const bodyMessageOriginal = data.mensagem;
 
-    return this.http.post<any>(url, formData, { headers: apiHeaders });
+    const bodyMessage = `<strong>Solicitante</strong>: ${bodyFromName} <br/> <strong>Destino</strong>: ${bodyMessageDestiny} <br/> <strong>Distrito</strong>: ${bodyMessageDiscricty} <br/> <strong>Telefone</strong>: ${bodyMessagePhone} <br/> <strong>Mensagem</strong>: ${bodyMessageOriginal}`.replace(/\r?\n/g, '<br/>');
+
+    const httpData = new FormData();
+    httpData.set('from', this.apiEmailOptions.emailFrom);
+    httpData.set('to', 'educa@rioclaro.rj.gov.br');
+    httpData.set('subject', `${bodyMessageType} - ${bodyFromName} - ${bodyMessageDiscricty}` );
+    httpData.set('h:Reply-To', `${bodyFromName} ${bodyFromEmail}`);
+    httpData.set('html', bodyMessage);
+
+    return this.serviceRequest.post<any>(httpUrl, httpData, { headers: apiHeaders }).pipe(
+      retry(1),
+      catchError(error => {
+        return throwError(`Error Code: ${error.status}\nMessage: ${error.message}`);
+      })
+    );
   }
 }
